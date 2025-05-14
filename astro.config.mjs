99bd536d90aece88658e07183c8f2c91aa325967
TheODDYSEY <mosessabz07@gmail.com>
2025-05-14 09:47:44 +0300

    updated site manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pawstronaut.netlify.app",
+	site: "https://whiskers-omega.vercel.app/",
 	base: "/",
 	integrations: [
 		tailwind(),
